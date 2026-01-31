e72a40f1f060b5c000369f19bf7684a9b06fa4bb
imoddev <iphonemod.net@gmail.com>
2026-01-31 18:17:09 +0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -2,5 +2,4 @@
 // Publishable key is safe to expose in the browser.
 
 export const SUPABASE_URL = "https://rayaztyesqxnbsxpvuvl.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_U8Wiel0nO-WspRxB09RLrQ_tYttf9BH";
-
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJheWF6dHllc3F4bmJzeHB2dXZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NTc4NzYsImV4cCI6MjA4NDAzMzg3Nn0.IExIv0gs5hf4yQVppXDZFS39H_SiQyymOpg-yW7Mc-g";
